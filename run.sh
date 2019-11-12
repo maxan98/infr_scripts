e2cf3df6a20b8eb2a6c7b4d5913bf7d81d9e3bd3
root <root@debianpxeserver>
2019-11-12 16:45:16 -0500

    \n on port parse fixed

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 echo "Hello, I will fetch IP addresses and other stuff. Just insert some info about port-forwarding."
 read -p 'dst-port: ' varname
 read -p 'to-port: ' varname1
-echo $varname>dstport.txt
-echo $varname1>toport.txt
+echo $varname | tr -dc '0-9'>dstport.txt
+echo $varname1 | tr -dc '0-9' >toport.txt
 #python vm_interactive.py && 
 cat last-vm-info.txt | grep "IP" > IP.txt && cat last-vm-info.txt | grep 'IP\|Guest\|State\|Name' > 2info.txt && cat IP.txt | tr -dc '0-9.' > 2IP.txt && ./nparse.sh 2info.txt  > 2name.txt
-
+python3 arg.py > mikrotik.conf
